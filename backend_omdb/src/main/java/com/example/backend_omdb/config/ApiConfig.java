7cee2550103a13d2337e59a891ae896fb4e075aa
Frank <frankmelendre14@gmail.com>
2025-01-28 14:21:10 +0100

    Add comments for ApiConfig

--- backend_omdb/src/main/java/com/example/backend_omdb/config/ApiConfig.java
+++ backend_omdb/src/main/java/com/example/backend_omdb/config/ApiConfig.java
@@ -1,25 +1,43 @@
 package com.example.backend_omdb.config;
 
-//Dotenv
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Clase de configuración para la API.
+ */
 @Configuration
 public class ApiConfig {
 
+    // Cargar las variables de entorno usando Dotenv
     private final Dotenv dotenv = Dotenv.load();
-    
+
+    /**
+     * Define un bean de RestTemplate para realizar solicitudes HTTP.
+     *
+     * @return Una instancia de RestTemplate.
+     */
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
+    /**
+     * Obtiene la URL de la API desde las variables de entorno.
+     *
+     * @return La URL de la API.
+     */
     public String getApiUrl() {
         return dotenv.get("OMDB_API_URL");
     }
 
+    /**
+     * Obtiene la clave de la API desde las variables de entorno.
+     *
+     * @return La clave de la API.
+     */
     public String getApiKey() {
         return dotenv.get("OMDB_API_KEY");
     }
