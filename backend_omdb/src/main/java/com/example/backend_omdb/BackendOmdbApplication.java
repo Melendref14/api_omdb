dcf211ca9a02267fc6487950d066f35929b0db3c
Frank <frankmelendre14@gmail.com>
2025-01-28 12:00:53 +0100

    Add initial backend_omdb project structure with Spring Boot and Maven configuration

---
+++ backend_omdb/src/main/java/com/example/backend_omdb/BackendOmdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_omdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOmdbApplication.class, args);
+	}
+
+}
---
+++ backend_omdb/src/test/java/com/example/backend_omdb/BackendOmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_omdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOmdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
