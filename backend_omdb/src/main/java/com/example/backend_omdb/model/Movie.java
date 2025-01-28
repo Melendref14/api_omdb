81aa87812d6ba3fb3b6752f2e94e7e18cc2a1a0b
Frank <frankmelendre14@gmail.com>
2025-01-28 15:23:27 +0100

    Add Movie class with attributes and accessor methods

---
+++ backend_omdb/src/main/java/com/example/backend_omdb/model/Movie.java
@@ -0,0 +1,51 @@
+package com.example.backend_omdb.model;
+
+public class Movie {
+
+    private String imdbID;
+    private String title;
+    private String year;
+    private String poster;
+    private int personalRating;
+
+    // Getters y setters
+    public String getImdbID() {
+        return imdbID;
+    }
+    
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public int getPersonalRating() {
+        return personalRating;
+    }
+
+    public void setPersonalRating(int personalRating) {
+        this.personalRating = personalRating;
+    }
+}
