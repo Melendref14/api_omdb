918b471932081edc3a36529378506b315896b0b2
Frank <frankmelendre14@gmail.com>
2025-01-31 17:08:30 +0100

    Update production environment configuration and dependencies for integration with Google Cloud SQL and CORS tuning

--- backend_omdb/src/main/java/com/example/backend_omdb/BackendOmdbApplication.java
+++ backend_omdb/src/main/java/com/example/backend_omdb/BackendOmdbApplication.java
@@ -1,5 +1,6 @@
 package com.example.backend_omdb;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,16 +12,21 @@ import org.springframework.lang.NonNull;
 public class BackendOmdbApplication {
 
     public static void main(String[] args) {
+        // Cargar las variables de entorno desde el archivo .env
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(BackendOmdbApplication.class, args);
     }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+        Dotenv dotenv = Dotenv.load();
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                    .allowedOrigins("http://localhost:3000", dotenv.get("FRONTEND_URL"))
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
