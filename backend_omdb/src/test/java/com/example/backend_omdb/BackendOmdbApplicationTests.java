326ade0935cd88138d4df8e93f0e491b92a9279a
Frank <frankmelendre14@gmail.com>
2025-02-03 00:27:56 +0100

    Setting system properties for the database using dotenv in testing

--- backend_omdb/src/test/java/com/example/backend_omdb/BackendOmdbApplicationTests.java
+++ backend_omdb/src/test/java/com/example/backend_omdb/BackendOmdbApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.backend_omdb;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendOmdbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void setup() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+    }
 
+    @Test
+    void contextLoads() {
+    }
 }
